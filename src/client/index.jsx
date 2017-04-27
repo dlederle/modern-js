548ca88b56f73b8bb13917d855b5cd3c1f8a3271
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2017-04-26 22:25:30 -0700

    seems broken

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -14,9 +14,10 @@ import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 
-// eslint-disable-next-line no-underscore-dangle
+/* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 const preloadedState = window.__PRELOADED_STATE__
+/* eslint-enable no-underscore-dangle */
 
 const store = createStore(
     combineReducers({ hello: helloReducer }),
